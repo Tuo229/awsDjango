194b9e5c63e816eb35ea568a50261819c0247221
Tuo229 <navaiguetuo1@gmail.com>
2021-06-07 18:13:47 +0000

    Delete models article

--- awsDemo/models.py
+++ awsDemo/models.py
@@ -1,16 +1,5 @@
 from django.db import models
 
-class Article(models.Model):
-
-    name = models.CharField("Nom de l'article", max_length=50)
-    title = models.CharField("Titre", max_length=254)
-
-    class Meta:
-        verbose_name = "Article"
-        verbose_name_plural = "Articles"
-
-    def __str__(self):
-        return self.name
 class Commande(models.Model):
 
     name = models.CharField("Nom de l'article", max_length=50)
