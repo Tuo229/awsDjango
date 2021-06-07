d3564f42b92cdb7987dc1a1088f97dd0f9445640
Tuo229 <navaiguetuo1@gmail.com>
2021-06-07 17:23:23 +0000

    add modif

--- awsDemo/models.py
+++ awsDemo/models.py
@@ -11,3 +11,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.name
+class Commande(models.Model):
+
+    name = models.CharField("Nom de l'article", max_length=50)
+    title = models.CharField("Titre", max_length=254)
+
+    class Meta:
+        verbose_name = "Commande"
+        verbose_name_plural = "Commandes"
+
+    def __str__(self):
+        return self.name
